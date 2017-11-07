6ecb3174da796b4cd6b456e89256fe95b6b00c73
denimars <marswandi.denilinux@gmail.com>
2017-11-07 08:20:34 +0800

    add new

--- app/controllers/pendidikan.go
+++ app/controllers/pendidikan.go
@@ -2,6 +2,11 @@ package controllers
 
 import(
 	"github.com/revel/revel"
+	"strings"
+	m "aptk/app/models"
+	"aptk/app/routes"
+	"aptk/app"
+	"log"
 )
 
 type Pendidikan struct{
@@ -10,4 +15,16 @@ type Pendidikan struct{
 
 func(c Pendidikan) Index() revel.Result{
 	return c.Render()
+}
+
+func(c Pendidikan) Save(list string) revel.Result{
+	var pendidikan m.Pendidikan
+	pendidikan.Id = 0
+	pendidikan.List = strings.ToUpper(list)
+	err := app.DB.Save(&pendidikan)
+	if err.Error!= nil{
+		log.Println(err)
+		panic(err)
+	}
+	return c.Redirect(routes.Pendidikan.Index())
 }
