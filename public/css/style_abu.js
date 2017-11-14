207ff25340b95244c464080a5be43bc88e08a42d
denimars <marswandi.denilinux@gmail.com>
2017-11-14 08:40:59 +0800

    add new

--- public/css/style_abu.js
+++ public/css/style_abu.js
@@ -2,14 +2,19 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   'navbar-default': {
-    'backgroundColor': '#235ebc',
-    'borderColor': '#180101',
-    'borderRadius': '0'
+    'backgroundColor': '#6c6878',
+    'borderColor': '#6c6878',
+    'borderRadius': '0',
+    'height': [{ 'unit': 'px', 'value': 38 }]
   },
   'navbar-default li a:hover': {
-    'color': '#1abc9c !important'
+    'color': '#ff711a !important'
   },
   'navbar-default navbar-nav>li>a': {
-    'color': '#cf1414'
+    'color': '#f6f6ff'
+  },
+  'head-head': {
+    'height': [{ 'unit': 'px', 'value': 76 }],
+    'backgroundColor': '#adcca8'
   }
 });
